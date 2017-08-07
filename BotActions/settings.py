4462d2d7fa602c120100785aa8e0ec05ecc7562a
Mohammad, Ibrahiem <ibrahiem.mohammad@fitchratings.com>
2017-08-07 11:15:43 -0500

    change settings

--- BotActions/settings.py
+++ BotActions/settings.py
@@ -1,7 +1,6 @@
-API_TOKEN="xoxb-211291446950-5YrD98Gbi8WV4wZScu9fv2Yi"
-WEB_HOOK="https://hooks.slack.com/services/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"
+API_TOKEN=""
+WEB_HOOK=""
 
 class ChannelsToPostTo:
     def __init__(self):
-        self.bot_testing_arena = '#bot_testing_arena'
-        self.testing_channel54 = '#testingchannel54'
+        self.bot_testing_arena = '#bot_testing_arena' #example
